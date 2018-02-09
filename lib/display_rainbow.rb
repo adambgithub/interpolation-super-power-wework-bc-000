446e8a58ad672187a0c949918bbe76c6ef0417cc
Adam Boyajian <adam.boyajian@wework.com>
2018-02-09 03:44:50 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+
+#array- colors = ["R", "O", "Y", "G", "B", "I", "V"]
+#array- colors(0)
+
+#colors = ["R", "O", "Y", "G", "B", "I", "V"]
+
+#def display_rainbow(colors)
+ # puts 
+#end
+
+#display_rainbow(colors)
+
+colors = ["R", "O", "Y", "G", "B", "I", "V"]
+
+puts colors(0)
